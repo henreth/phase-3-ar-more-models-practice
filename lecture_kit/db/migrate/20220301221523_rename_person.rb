06a1d7865438b4aef87aa9d93234753006fdfa48
Henry Miller <mail.henrym+git@gmail.com>
2022-03-01 16:29:34 -0600

    first deliverable

---
+++ lecture_kit/db/migrate/20220301221523_rename_person.rb
@@ -0,0 +1,5 @@
+class RenamePerson < ActiveRecord::Migration[5.2]
+  def change
+    rename_table :plant_parents, :people
+  end
+end
---
+++ lecture_kit/db/migrate/20220301222622_rename_plant_parent_id.rb
@@ -0,0 +1,5 @@
+class RenamePlantParentId < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :plant_parenthoods, :plant_parent_id, :person_id
+  end
+end
